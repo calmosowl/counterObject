05d03080b557156c1493b6b30318192769fb4b29
Artem Kiselev <artem.ki@devcaz.com>
2017-05-16 14:19:22 +0300

    clean

---
+++ counter.js
@@ -0,0 +1,99 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", (ev) => {
+	(()=>{
+		let a = new JackPotCounter({
+			tickLength: 1000,
+			qtyPerTick: 1
+		});
+		
+		let output = document.getElementById('output'),
+			play = document.getElementById('tickPlay'),
+			stop = document.getElementById('tickStop'),
+			get = document.getElementById('getValue');
+
+		
+		play.onclick= (ev) => {
+			ev.preventDefault();
+			a.tickPlay().echo(output);		
+		};
+		stop.onclick= (ev) => {
+			ev.preventDefault();
+			a.tickStop().echoStop();			
+		};
+
+		get.onclick = (ev) => {
+			ev.preventDefault();
+			alert(a.getCurrentValue());
+		}
+
+	})();
+}, false);
+
+
+
+function JackPotCounter(options){
+	let that = this;
+	let bufferPrototype = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+	this.buffer = bufferPrototype;
+	this.data = {
+		latest:0,
+		diffBetweenLatests:0,
+		groupAverage: 0,
+		time: 0
+	};
+	this.currentValue = options&&options.value ? options.value : 0;
+	this.ticker = [];
+	this.echoTicker = [];
+	this.tickLength = options&&options.tickLength ? options.tickLength : 1000;
+	this.qtyPerTick = options&&options.qtyPerTick ? options.qtyPerTick : 1;
+	this.tickPlay = () => {
+		if(this.ticker.length < 1){
+			this.ticker[0] = setInterval(()=>{
+				this.buffer.splice(0, 0, this.currentValue);
+				this.buffer.length = 10;
+				this.data.latest = this.currentValue;
+				this.data.diffBetweenLatests = +(this.buffer[0]-this.buffer[1]).toFixed(2);
+				//this.data.groupAverage = +(this.buffer[this.buffer.length-1] / this.buffer.length).toFixed(2);
+				this.data.time = Date.now();
+				this.data.arrayStamp = this.buffer.join(', ');
+				this.currentValue = rough(+(this.currentValue + this.qtyPerTick).toFixed(2));
+				this.dev();
+			}, this.tickLength);
+		};
+		return this;
+	};
+	this.dev = () => {
+		console.log(this.data);
+	    return this;
+	};
+	this.echo = ($) => {
+		this.echoTicker[0] = setInterval(()=>{
+			$.innerHTML = that.currentValue;
+		},1000);
+		return this;
+	};
+	this.echoStop = () => {
+		clearInterval(this.echoTicker);
+		this.echoTicker = [];
+		return this;
+	};
+	this.tickStop = () => {
+		clearInterval(this.ticker);
+		this.ticker = [];
+		return this;
+	};
+	this.getCurrentValue = () => {
+		return this.currentValue;
+	};
+	return this;
+};
+
+function rough(value, factor){
+	if(isNaN(value/2)) return false;
+
+	factor = factor || 1; 
+	let res = Math.random() >= .5 ? value + (Math.random()*factor) : value - (Math.random()*factor);
+
+	res = +(res).toFixed(2);
+	return res;
+};
